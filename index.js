4d9480018d2a38f5b53b3eb015c4b2a3c0412ccf
Dharmesh <Dharmesh@example.com>
2023-12-24 21:47:59 +0530

    Code change

--- index.js
+++ index.js
@@ -188,7 +188,12 @@ const saveEditChanges = (event) => {
 
 };
 
-const testPRCode = (num1, num2) => num1 + num2;
+const valueCheck = (num1, num2) => {
+  if (num1 > num2) {
+    return "Num1 is Bigger then Num2";
+  }
+  return "Num1 is Smaller then Num2";
+}
 
 
 
